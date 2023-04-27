9fa78d1252715e378a00606361c68fc1fea315ea
Your Name <geralddizon84@yahoo.com>
2023-04-27 14:39:17 +0800

    04-27 Update

---
+++ index.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+    $(function () {
+        $(document).scroll(function () {
+            var $nav = $(".fixed-top");
+            var $logo = $(".navbar-brand");
+            $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+            $logo.toggleClass('colored', $(this).scrollTop() > $nav.height());
+        });
+    });
+});
